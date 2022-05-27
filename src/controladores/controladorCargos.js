539dc6a3b24cddea0b6c989eca339a6c52eb8b19
Caste <www.jrctabora@gmail.com>
2022-05-26 18:49:27 -0600

    CRUD Cargos

--- src/controladores/controladorCargos.js
+++ src/controladores/controladorCargos.js
@@ -11,3 +11,101 @@ exports.Listar = async(req, res) => {
         res.json(error);
     }
 }
+
+exports.Guardar = async (req, res) => {
+    const validaciones = validationResult(req);
+    console.log(validaciones.errors);
+    const msj = {
+        mensaje: ''
+    };
+    if (validaciones.errors.length > 0) {
+        validaciones.errors.forEach(error => {
+            msj.mensaje += error.msg + '. ';
+        });
+    }else{
+        const {nombre, descripcion} = req.body;
+        try {            
+            await modeloCargo.create({
+                nombre: nombre,
+                descripcion: descripcion
+            });
+            msj.mensaje = 'Cargo guardado correctamente';
+        } catch (error) {
+            msj.mensaje = 'Error al guardar los datos';
+        }
+    }
+    res.json(msj.mensaje);
+}
+
+exports.Editar = async (req, res) => {
+    const validaciones = validationResult(req);
+    console.log(validaciones.errors);
+    const msj = {
+        mensaje: ''
+    };
+    if (validaciones.errors.length > 0) {
+        validaciones.errors.forEach(error => {
+            msj.mensaje += error.msg + '. ';
+        });
+    }else{
+        const {id} = req.query;
+        const {nombre, descripcion} = req.body;
+        try { 
+            var buscarCargo = await modeloCargo.findOne({
+                where: {
+                    id: id
+                }
+            });       
+            if(!buscarCargo){
+                msj.mensaje = 'No se ha encontrado un cargo con el ID ' + id;
+            }
+            else{
+                buscarCargo.nombre = nombre;
+                buscarCargo.descripcion = descripcion;
+                await buscarCargo.save();
+
+                
+                msj.mensaje = 'Cargo actualizado correctamente';
+            }
+        } catch (error) {
+            msj.mensaje = 'Error al guardar los datos';
+        }
+    }
+    res.json(msj.mensaje);
+}
+
+exports.Eliminar = async (req, res) => {
+    const validaciones = validationResult(req);
+    console.log(validaciones.errors);
+    const msj = {
+        mensaje: ''
+    };
+    if (validaciones.errors.length > 0) {
+        validaciones.errors.forEach(error => {
+            msj.mensaje += error.msg + '. ';
+        });
+    }else{
+        const {id} = req.query;
+        try { 
+            var buscarCargo = await modeloCargo.findOne({
+                where: {
+                    id: id
+                }
+            });       
+            if(!buscarCargo){
+                msj.mensaje = 'No se ha encontrado un cargo con el ID ' + id;
+            }
+            else{
+                await buscarCargo.destroy({
+                    where: {
+                        id: id
+                    }
+                });
+                msj.mensaje = 'Cargo eliminado correctamente';
+            }
+        } catch (error) {
+            msj.mensaje = 'Error al borrar el cargo';
+        }
+    }
+    res.json(msj.mensaje);
+}
--- src/rutas/rutasCargos.js
+++ src/rutas/rutasCargos.js
@@ -5,40 +5,30 @@ const controladorCargos = require('../controladores/controladorCargos');
 const rutas = Router();
 rutas.get('/listar', controladorCargos.Listar);
 
-/* 
-rutas.post('/',
-body ('usuario')
-.notEmpty().withMessage('Debe enviar el usuario')
-.isLength({min:3}).withMessage('El usuario debe tener al menos 3 caracteres')
-,
-body ('contrasena')
-.notEmpty().withMessage('Debe enviar la contraseña')
-.isLength({min:6}).withMessage('La contraseña debe tener al menos 6 caracteres')
-,
-
-controladorInicio.EjemploPost);
 
 
-rutas.put('/',
+rutas.post('/guardar',
+body ('nombre')
+.notEmpty().withMessage('Debe enviar el nombre del cargo')
+.isLength({min:3}).withMessage('El nombre del cargo debe tener al menos 3 caracteres'),
+controladorCargos.Guardar);
+
+
+rutas.put('/editar',
 query('id')
-.notEmpty().withMessage('El ID del Usuario no puede estar vacio')
-.isInt().withMessage('El ID del Usuario debe ser un numero entero'),
+.notEmpty().withMessage('El ID del cargo no puede estar vacio')
+.isInt().withMessage('El ID del cargo debe ser un numero entero'),
 
-body ('usuario')
-.notEmpty().withMessage('Debe enviar el usuario')
-.isLength({min:3}).withMessage('El usuario debe tener al menos 3 caracteres')
-,
-body ('contrasena')
-.notEmpty().withMessage('Debe enviar la contraseña')
-.isLength({min:6}).withMessage('La contraseña debe tener al menos 6 caracteres')
-,
-controladorInicio.EjemploPut);
+body ('nombre')
+.notEmpty().withMessage('Debe enviar el nombre del cargo')
+.isLength({min:3}).withMessage('El nombre del cargo debe tener al menos 3 caracteres'),
+controladorCargos.Editar);
 
-//* Delete
-rutas.delete('/',
+//*Delete
+rutas.delete('/eliminar',
 query('id')
-.notEmpty().withMessage('El ID del Usuario no puede estar vacio')
-.isInt().withMessage('El ID del Usuario debe ser un numero entero'),
-controladorInicio.EjemploDelete);
- */
+.notEmpty().withMessage('El ID del cargo no puede estar vacio')
+.isInt().withMessage('El ID del cargo debe ser un numero entero'),
+controladorCargos.Eliminar);
+
 module.exports = rutas;
