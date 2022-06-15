4a6ff59ac1c303a3c644684e562cc8fa11a87f3c
Caste <www.jrctabora@gmail.com>
2022-06-14 18:20:36 -0600

    Relacionar archivo con empleado

--- src/controladores/controladorArchivo.js
+++ src/controladores/controladorArchivo.js
@@ -1,8 +1,27 @@
 const fs = require('fs'); //File System
 const path = require('path'); //Path
 const Empleado = require('../modelos/modeloEmpleado'); //Empleado
+const msj = require('../componentes/mensajes'); //
 
 exports.Recibir = async (req, res) => {
+    const {filename} = req.file;
+    const {id} = req.body;
+    try {
+        var buscarEmpleado = await modeloEmpleado.findOne({
+            where: {
+                id: id
+            }
+        })
+        if(!buscarEmpleado){
+            const buscarImagen = fs.existsSync(path.join(__dirname,'../public/img/empleados/'+filename));
+            if(buscarImagen){
+                
+            } 
+        }
+    } catch (error) {
+        
+    }
+
     console.log(req.file);
     res.send('Archivo guardado')
 }
